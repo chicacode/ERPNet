361bc61d276e2b93f7d7f58359ef3ac9d8e884b1
gerihal <gerahernan.al@gmail.com>
2020-10-15 13:14:53 +0200

    Adding each repository by entity

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -52,8 +52,7 @@ namespace ERPNet.Controllers
         }
 
         //// PUT: api/Employees/5
-        //// To protect from overposting attacks, enable the specific properties you want to bind to, for
-        //// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+    
         [HttpPut ( "edit/{id}" )]
         public async Task<IActionResult> EditEmployee (  Employee employee )
         {
---
+++ Data/Repositories/AddressesRepository.cs
@@ -0,0 +1,27 @@
+﻿using ERPNet.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPNet.Data.Repositories
+{
+    public class AddressesRepository : GenericRepository<Address, ERPNetContext>
+    {
+        private readonly ERPNetContext _context;
+
+        public AddressesRepository ( ERPNetContext context ) :base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Address> GetAddress (int id )
+        {
+            return await _context.Address
+                .SingleOrDefaultAsync ( a => a.Id == id );
+        }
+
+
+    }
+}
