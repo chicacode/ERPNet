cb80f93a952c93879703f4748a6b0f2d157301dc
gerihal <gerahernan.al@gmail.com>
2020-10-17 13:43:57 +0200

    Added Method without Password

---
+++ Helpers/ExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using ERPNet.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPNet.Helpers
+{
+    public static class ExtensionMethods
+    {
+        public static IEnumerable<User> WithoutPasswords(this IEnumerable<User> users )
+        {
+            if(users == null) return null;
+            return users.Select ( x => x.WithoutPassword () );
+        }
+
+        public static User WithoutPassword(this User user )
+        {
+            if(user == null) return null;
+
+            user.Password = null;
+            return user;
+        }
+    }
+}
