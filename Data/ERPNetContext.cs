76ebdbb69d47bb085bcc4516b854f3915a3bfa60
gerihal <gerahernan.al@gmail.com>
2020-10-27 16:49:28 +0100

    refactor

--- Data/ERPNetContext.cs
+++ Data/ERPNetContext.cs
@@ -1,6 +1,4 @@
-﻿
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using ERPNet.Models;
 using ERPNet.Entities;
 
--- Data/Repositories/OrderRepository.cs
+++ Data/Repositories/OrderRepository.cs
@@ -1,6 +1,5 @@
 ﻿using ERPNet.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
