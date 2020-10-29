1dabb458962a74d5d3d7865f58c4282f94f99f50
gerihal <gerahernan.al@gmail.com>
2020-10-29 10:48:59 +0100

    added detail error key in program

--- Program.cs
+++ Program.cs
@@ -44,7 +44,8 @@ namespace ERPNet
             Host.CreateDefaultBuilder ( args )
                 .ConfigureWebHostDefaults ( webBuilder =>
                   {
-                      webBuilder.UseStartup<Startup> ();
+                      webBuilder.UseStartup<Startup> ()
+                      .UseSetting ( WebHostDefaults.DetailedErrorsKey, "true" );
                   } );
     }
 }
